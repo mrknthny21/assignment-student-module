8574b9823344ad4fe961a44540fc9ff4f77e10cb
mrknthny21 <markanthonybasmayor0416@gmail.com>
2025-01-07 18:38:18 +0800

    read students function

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,17 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './student.entity';
 
-@Controller('students')
+@Controller('students')  // Make sure this is the right route
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  // Endpoint to create a student
-  @Post()
-  async create(@Body() body: { firstName: string, lastName: string, email: string, enrollmentDate: string }): Promise<Student> {
-    const { firstName, lastName, email, enrollmentDate } = body;
-
-    // Create the student by passing the data directly
-    return this.studentService.createStudent(firstName, lastName, email, new Date(enrollmentDate));
+  // Handle GET request to fetch all students
+  @Get()
+  async getAllStudents(): Promise<Student[]> {
+    return this.studentService.getAllStudents(); // Call service method to get students
   }
 }
