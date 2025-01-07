93a6afe8dac35cd13024cfd9d6c8ad3fd1aaa178
mrknthny21 <markanthonybasmayor0416@gmail.com>
2025-01-07 18:48:02 +0800

    update students function

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Student } from './student.entity';
@@ -10,9 +10,16 @@ export class StudentService {
     private studentRepository: Repository<Student>,
   ) {}
 
-  async getAllStudents(): Promise<Student[]> {
-    return this.studentRepository.find();
+  async updateStudent(id: number, updateData: Partial<Student>): Promise<Student> {
+    const student = await this.studentRepository.findOne({ where: { id } });
+
+    if (!student) {
+      throw new NotFoundException(`Student with ID ${id} not found`);
+    }
+
+    // Update the existing student data
+    Object.assign(student, updateData);
+
+    return this.studentRepository.save(student); // Save the updated student back to the DB
   }
-
-
 }
